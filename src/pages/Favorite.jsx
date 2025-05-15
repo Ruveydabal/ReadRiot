ac102b55a725f669e668868159b51abec023aed5
anke180 <153827509+anke180@users.noreply.github.com>
2025-05-15 15:27:43 +0200

    New files and Data
    Made new files with the incomplete Nav and de data works with the firebase. Je krijgt het nog niet in beeld maar het staat wel in de console

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,12 @@
+import Nav from "./Nav.jsx";
+
+const Favorites = () => {
+
+    return (
+        <div>
+            <Nav />
+        </div>
+    )   
+}
+
+export default Favorites;
---
+++ src/pages/Friends.jsx
@@ -0,0 +1,12 @@
+import Nav from "./Nav.jsx";
+
+const Friends = () => {
+
+    return (
+        <div>
+            <Nav />
+        </div>
+    )   
+}
+
+export default Friends;
