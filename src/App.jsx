bc434040c1c990c3501578f3d55b3bc5a96fc52a
anke180 <153827509+anke180@users.noreply.github.com>
2025-06-11 11:43:16 +0200

    Too much
    I did it too much, so I had to bring this back

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
 import Friends from "./pages/Friends.jsx";
 import Login from "./pages/Login.jsx";
+import AddBoek from "./pages/AddBoek.jsx";
 
 const App = () => {
     return (
         <Routes>
             <Route path={"/"} element={<Home />} />
-            {/* <Route path={"/addbook"} element={<AddBoek />} /> */}
+            <Route path={"/addbook"} element={<AddBoek />} />
             <Route path={"/books"} element={<Books />} />
             <Route path={"/friends"} element={<Friends />} />
             <Route path={"/login"} element={<Login />} />
